ad81e5907f068d9da385adb22ec17827d3de7f51
Laurent Barbe <lbarbe@ccmbenchmark.com>
2017-02-03 10:22:37 +0100

    cleanup indent

--- autosnap.sh
+++ autosnap.sh
@@ -18,77 +18,72 @@
 # Config file
 configfile=/etc/ceph/scripts/autosnap.conf
 if [ ! -f $configfile ]; then
-	echo "Config file not found $configfile"
-	exit 0
+    echo "Config file not found $configfile"
+    exit 0
 fi
 source $configfile
 
 
 log() {
-	[[ "$verbose" = "yes" ]] && echo -ne $1
-	return 0
+    [[ "$verbose" = "yes" ]] && echo -ne $1
+    return 0
 }
 
 
 makesnapshot() {
-	share=$1
+    share=$1
 
-	snapname=`date -u +GMT-%Y.%m.%d-%H.%M.%S-autosnap`
+    snapname=`date -u +GMT-%Y.%m.%d-%H.%M.%S-autosnap`
 
-	log "* Create snapshot for $share: @$snapname\n"
-	[[ "$useenhancedio" = "yes" ]] && {
-		/sbin/sysctl dev.enhanceio.$share.do_clean=1
-		while [[ `cat /proc/sys/dev/enhanceio/$share/do_clean` == 1 ]]; do sleep 1; done
-	}
-	mountpoint -q $sharedirectory/$share \
-		&& sync \
-		&& log "synced, " \
-		&& xfs_freeze -f $sharedirectory/$share \
-		&& [[ "$useenhancedio" = "yes" ]] && {
-				/sbin/sysctl dev.enhanceio.$share.do_clean=1
-				while [[ `cat /proc/sys/dev/enhanceio/$share/do_clean` == 1 ]]; do sleep 1; done
-				log "wb cache cleaned, "
-			} \
-			|| log "no cache, " \
-		&& rbd --id=$id --keyring=$keyring snap create $rbdpool/$share@$snapname \
-		&& log "snapshot created.\n" 
-	xfs_freeze -u $sharedirectory/$share
+    log "\* Create snapshot for $share: @$snapname\n"
+    [[ "$useenhancedio" = "yes" ]] && {
+        /sbin/sysctl dev.enhanceio.$share.do_clean=1
+        while [[ `cat /proc/sys/dev/enhanceio/$share/do_clean` == 1 ]]; do sleep 1; done
+    }
+    mountpoint -q $sharedirectory/$share \
+        && sync \
+        && log "synced, " \
+        && xfs_freeze -f $sharedirectory/$share \
+        && [[ "$useenhancedio" = "yes" ]] && {
+                /sbin/sysctl dev.enhanceio.$share.do_clean=1
+                while [[ `cat /proc/sys/dev/enhanceio/$share/do_clean` == 1 ]]; do sleep 1; done
+                log "wb cache cleaned, "
+            } \
+            || log "no cache, " \
+        && rbd --id=$id --keyring=$keyring snap create $rbdpool/$share@$snapname \
+        && log "snapshot created.\n" 
+    xfs_freeze -u $sharedirectory/$share
 
 }
 
 
 mountshadowcopy() {
-	share=$1
-	shadowcopylist=""
-	# GET ALL EXISTING SNAPSHOT ON RBD
-	snapcollection=$(rbd --id=$id --keyring=$keyring snap ls $rbdpool/$share | awk '{print $2}' | grep -- 'GMT-.*-autosnap$' | sort | sed 's/-autosnap$//g')
+    share=$1
+    shadowcopylist=""
+    # GET ALL EXISTING SNAPSHOT ON RBD
+    snapcollection=$(rbd --id=$id --keyring=$keyring snap ls $rbdpool/$share | awk '{print $2}' | grep -- 'GMT-.*-autosnap$' | sort | sed 's/-autosnap$//g')
 
-	datestart=$(date -u +%Y.%m.%d -d "$daterange")
-        echo "start $datestart"
-        dateend=$(date -u +%Y.%m.%d)
-        echo "end $dateend"
+    datestart=$(date -u +%Y.%m.%d -d "$daterange")
+    dateend=$(date -u +%Y.%m.%d)
 
-        for snapitem in $(echo "$snapcollection")
-        do
-            timestamp=$(echo "$snapitem"|awk -F- '{print $2}')
-            # Do inclusive range check
-	    if [[ "$timestamp" == "$datestart" ||
+    for snapitem in $(echo "$snapcollection")
+    do
+        timestamp=$(echo "$snapitem"|awk -F- '{print $2}')
+        # Do inclusive range check
+        if [[ "$timestamp" == "$datestart" ||
                         "$timestamp" >  "$datestart" && "$timestamp" <  "$dateend" ||
                         "$timestamp" == "$dateend" ]]
             then
-                echo "$snapitem in range, adding to mount list."
                 shadowcopylist=$(echo "$shadowcopylist $snapitem")
-            else
-                echo "$snapitem not in range, ignoring."
             fi
-	done
+    done
 
-	# Shadow copies to mount
-	log "* Shadow Copies to mount for $rbdpool/$share :\n$shadowcopylist\n" | sed 's/^$/-/g'
+    # Shadow copies to mount
+    log "\* Shadow Copies to mount for $rbdpool/$share :\n$shadowcopylist\n" | sed 's/^$/-/g'
 
-	# GET MOUNTED SNAP
-	[ ! -d $sharedirectory/$share/.snapshots ] && echo "Snapshot directory $sharedirectory/$share/.snapshots does not exist. Please create it before run." && return
-	snapmounted=$(mount|grep $sharedirectory/$share/.snapshots|awk '{print $3}'|awk -F/ '{print $NF}'|sed 's/^@//g')
+    # GET MOUNTED SNAP
+    [ ! -d $sharedirectory/$share/.snapshots ] && echo "Snapshot directory $sharedirectory/$share/.snapshots does not exist. Please create it before run." && return
+    snapmounted=$(mount|grep $sharedirectory/$share/.snapshots|awk '{print $3}'|awk -F/ '{print $NF}'|sed 's/^@//g')
         # Cleanup abandoned snap mountpoints                                                                                                                                                                      
         for mountdir in $(ls "$sharedirectory/$share/.snapshots"|sed 's/^@//g')
         do
@@ -99,67 +94,67 @@ mountshadowcopy() {
             fi
         done
 
-	# Umount Snapshots not selected in shadowcopylist
-	for snapshot in $snapmounted; do
-		mountdir=$sharedirectory/$share/.snapshots/@$snapshot
-		echo "$shadowcopylist" | grep -q "$snapshot" || {
-			umount $mountdir \
-			&& rmdir $mountdir \
-			&& rbd unmap /dev/rbd/$rbdpool/$share@$snapshot-autosnap
-		}
-	done
+    # Umount Snapshots not selected in shadowcopylist
+    for snapshot in $snapmounted; do
+        mountdir=$sharedirectory/$share/.snapshots/@$snapshot
+        echo "$shadowcopylist" | grep -q "$snapshot" || {
+            umount $mountdir \
+            && rmdir $mountdir \
+            && rbd unmap /dev/rbd/$rbdpool/$share@$snapshot-autosnap
+        }
+    done
 
-	# Mount snaps in $shadowcopylist not already mount
-	for snapshot in $shadowcopylist; do
-		mountdir=$sharedirectory/$share/.snapshots/@$snapshot
-		mountpoint -q $mountdir || {
-			[ ! -d $mountdir ] && mkdir $mountdir
-			rbd showmapped | awk '{print $4}' | grep "^$" || rbd --id=$id --keyring=$keyring map $rbdpool/$share@$snapshot-autosnap > /dev/null
-			mount $mntoptions /dev/rbd/$rbdpool/$share@$snapshot-autosnap $mountdir
-		}
-	done
+    # Mount snaps in $shadowcopylist not already mount
+    for snapshot in $shadowcopylist; do
+        mountdir=$sharedirectory/$share/.snapshots/@$snapshot
+        mountpoint -q $mountdir || {
+            [ ! -d $mountdir ] && mkdir $mountdir
+            rbd showmapped | awk '{print $4}' | grep "^$" || rbd --id=$id --keyring=$keyring map $rbdpool/$share@$snapshot-autosnap > /dev/null
+            mount $mntoptions /dev/rbd/$rbdpool/$share@$snapshot-autosnap $mountdir
+        }
+    done
 
 }
 
 cleansnapshot() {
-	share=$1
+    share=$1
 
-	snapcollection=$(rbd --id=$id --keyring=$keyring snap ls $rbdpool/$share | awk '{print $2}' | grep -- 'GMT-.*-autosnap$' | sort )
+    snapcollection=$(rbd --id=$id --keyring=$keyring snap ls $rbdpool/$share | awk '{print $2}' | grep -- 'GMT-.*-autosnap$' | sort )
 
-	for snapshot in $snapcollection; do
-		datesnap=`echo $snapshot | cut -f2 -d'-'`
-		if [[ "$datesnap" < `date -u +%Y.%m.%d -d "$snapretention day ago"` ]]; then
-			if ! ( [[ "$keepmonthlyretention" = "yes" ]] && [[ "$datesnap" = *.01 ]] ); then
-				log "* Delete snapshot for $share: @$snapshot\n"
-				rbd --id=$id --keyring=$keyring snap rm $rbdpool/$share@$snapshot
-				sleep 600
-			fi
-		fi
-	done
+    for snapshot in $snapcollection; do
+        datesnap=`echo $snapshot | cut -f2 -d'-'`
+        if [[ "$datesnap" < `date -u +%Y.%m.%d -d "$snapretention day ago"` ]]; then
+            if ! ( [[ "$keepmonthlyretention" = "yes" ]] && [[ "$datesnap" = *.01 ]] ); then
+                log "\* Delete snapshot for $share: @$snapshot\n"
+                rbd --id=$id --keyring=$keyring snap rm $rbdpool/$share@$snapshot
+                sleep 600
+            fi
+        fi
+    done
 
 }
 
 
 if [[ "$snapshotenable" = "yes" ]]; then
-	log "---- SNAPSHOT ENABLED ----\n"
-	for share in $sharelist; do
-		makesnapshot $share
-	done
+    log "---- SNAPSHOT ENABLED ----\n"
+    for share in $sharelist; do
+        makesnapshot $share
+    done
 fi
 
 [[ "$snapshotenable" = "yes" ]] && [[ "$mountshadowcopyenable" = "yes" ]] && sleep 60
 
 if [[ "$mountshadowcopyenable" = "yes" ]]; then
-	log "---- MOUNT SHADOW COPY ENABLED ----\n"
-	for share in $sharelist; do
-		mountshadowcopy $share
-	done
+    log "---- MOUNT SHADOW COPY ENABLED ----\n"
+    for share in $sharelist; do
+        mountshadowcopy $share
+    done
 fi
 
 if [[ "$cleansnapshotenable" = "yes" ]]; then
-	log "---- CLEAN SNAPSHOT ENABLED ----\n"
-	for share in $sharelist; do
-		cleansnapshot $share
-	done
+    log "---- CLEAN SNAPSHOT ENABLED ----\n"
+    for share in $sharelist; do
+        cleansnapshot $share
+    done
 fi
 
